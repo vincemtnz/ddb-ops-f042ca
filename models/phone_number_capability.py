95c409c509141706948cd9c683c615f68ce6ceb6
Vince Martinez <183071+vincemtnz@users.noreply.github.com>
2025-02-06 10:26:03 +0000

    Use botocore.session

--- models/phone_number_capability.py
+++ models/phone_number_capability.py
@@ -1,17 +1,27 @@
+import os
+import sys
+from botocore.session import Session
 from pynamodb.models import Model
 from pynamodb.attributes import UnicodeAttribute, BooleanAttribute
-from os import environ
 from dotenv import load_dotenv
 
 # Load environment variables from .env file, if present.
 # We're mainly using
 load_dotenv()
 
+TABLE_NAME=os.environ.get("TABLE_NAME")
+
+botocore_session = Session()
+
 class PhoneNumberCapability(Model):
     class Meta: # pyright: ignore [reportIncompatibleVariableOverride]
-        table_name = environ.get("TABLE_NAME")
+        table_name = TABLE_NAME
+        botocore_session = botocore_session
 
     phone_number = UnicodeAttribute(hash_key=True)
     channel = UnicodeAttribute(range_key=True)
     is_capable = BooleanAttribute(default=True)
     last_refreshed_at = UnicodeAttribute()
+
+if not TABLE_NAME:
+    sys.exit("TABLE_NAME environment variable not set")
